0cb102dbe0084b7699d26fe14fa29ff3a731aa10
Sourav Sarkar <djsouravcoc@gmail.com>
2026-03-20 22:43:22 +0530

    Day-11

--- Array/25_Second largest.cpp
+++ Array/25_Second largest.cpp
@@ -22,3 +22,32 @@ class Solution {
 Time Complexity: O(N) + O(N) = O(2N) => O(N)
 Space Complexity: O(1)
 */
+
+//Optimal Solution - One Pass
+
+class Solution {
+  public:
+    int getSecondLargest(vector<int> &arr) {
+        int n = arr.size();
+        int largest = -1 , second_largest = -1;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]>largest)
+            {
+                second_largest = largest;
+                largest = arr[i];
+            }
+            else if(arr[i]<largest && arr[i]>second_largest)
+            {
+                second_largest = arr[i];
+            }
+        }
+        return second_largest;
+        
+    }
+};
+
+/*
+Time Complexity: O(N)
+Space Complexity: O(1)
+*/
