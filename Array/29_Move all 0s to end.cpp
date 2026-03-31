c28b214833ce684224137ec15bc5c56690d06910
Sourav Sarkar <djsouravcoc@gmail.com>
2026-03-31 23:36:38 +0530

    Day-16

--- Array/29_Move all 0s to end.cpp
+++ Array/29_Move all 0s to end.cpp
@@ -1,3 +1,40 @@
+/*
+If we take another array to rearrange the elements, then we can easily solve this problem. But that will require O(N) extra space. We can solve this problem in O(1) extra space by using the two-pointer approach.
+*/
+/*Another array approach*/
+
+/*
+#include<iostream>
+using namespace std;
+void movezero(int arr[],int n)
+{
+    int newarr[n];
+    int j = 0;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]!=0)
+        {
+            newarr[j++]=arr[i];
+        }
+    }
+    //Append zeros
+    while(j<n)
+    {
+        newarr[j++] = 0;
+    }
+    
+}
+int main()
+{
+    int arr[] = {5,0,2,9,0,4};
+    int n = sizeof(arr[])/sizeof(arr[0]);
+    int ans = movezero(arr,n);
+    cout<<ans;
+    return 0;
+}
+TC: O(N) SC: O(N)
+*/
+/*Two-pointer approach*/
 #include<bits/stdc++.h>
 using namespace std;
 class Solution {
